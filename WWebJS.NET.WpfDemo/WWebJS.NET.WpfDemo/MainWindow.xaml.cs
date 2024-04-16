de77b09002fcc00dd8e5616bb937a3215750b8c2
yassinMi <dir16cat17@gmail.com>
2024-04-16 13:07:35 +0000

    initial, a wpf demo project

---
+++ WWebJS.NET.WpfDemo/WWebJS.NET.WpfDemo/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WWebJS.NET.WpfDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            button1.Click += async(s, e) => {
+
+                try
+                {
+                    
+                    textBlock1.Text = "getting result....";
+                }
+                catch (Exception err)
+                {
+                    textBlock1.Text =err.ToString();
+                      
+                }
+                
+                
+            };
+        }
+    }
+}
